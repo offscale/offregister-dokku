26dc771675c9d429ad5db37cc0c5ba1d3528baf0
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-09-19 12:15:47 +1000

    [setup.py] Fix for Python 3; [*.py] Minor improvements to Python 3

--- offregister_dokku/ubuntu.py
+++ offregister_dokku/ubuntu.py
@@ -1,4 +1,10 @@
-from io import StringIO
+from sys import version
+
+if version[0] == "2":
+    from cStringIO import StringIO
+
+else:
+    from io import StringIO
 from types import BooleanType
 from os import environ, path
 
