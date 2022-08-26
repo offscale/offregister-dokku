04d2b4d700627162bbd71d55a392ab6c8a8fa95d
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-08-25 22:06:30 -0400

    [*.py] Update to threadsafe Python 3 compatible Fabric ; [README.md] Update support Python version range ; [LICENSE-{APACHE,MIT}] Update copyright year range

--- offregister_dokku/ubuntu.py
+++ offregister_dokku/ubuntu.py
@@ -10,8 +10,7 @@ else:
 
 from os import environ, path
 
-from fabric.api import run, sudo
-from fabric.contrib.files import append, exists, put, upload_template
+from fabric.contrib.files import append, exists
 from offregister_fab_utils.apt import apt_depends
 from offregister_fab_utils.fs import cmd_avail
 from offregister_fab_utils.git import clone_or_update, url_to_git_dict
@@ -19,7 +18,7 @@ from offregister_fab_utils.ubuntu import docker
 from pkg_resources import resource_filename
 
 
-def step0(domain, *args, **kwargs):
+def step0(c, domain, *args, **kwargs):
     key_file = "/root/.ssh/id_rsa.pub"
     config = {
         "DOKKU_HOSTNAME": ("hostname", domain),
@@ -37,23 +36,23 @@ def step0(domain, *args, **kwargs):
         "DOKKU_PUBLIC_KEY_PATH", environ["PUBLIC_KEY_PATH"]
     )
 
-    if not cmd_avail("docker"):
-        docker.install_0()
+    if not cmd_avail(c, "docker"):
+        docker.install_0(c)
         # docker.dockeruser_1()
-        docker.serve_2()
+        docker.serve_2(c)
 
-    put(StringIO("pZPlHOkV649DCepEwf9G"), "/tmp/passwd")
+    c.put(StringIO("pZPlHOkV649DCepEwf9G"), "/tmp/passwd")
 
-    if not cmd_avail("dokku"):  # is_installed('dokku'):
-        run("wget -qN https://packagecloud.io/gpg.key")
-        sudo("apt-key add gpg.key")
+    if not cmd_avail(c, "dokku"):  # is_installed('dokku'):
+        c.run("wget -qN https://packagecloud.io/gpg.key")
+        c.sudo("apt-key add gpg.key")
         append(
             "/etc/apt/sources.list.d/dokku.list",
             "deb https://packagecloud.io/dokku/dokku/ubuntu/ trusty main",
             use_sudo=True,
         )
 
-        put(
+        c.put(
             StringIO(
                 "\n".join(
                     "{com} {com}/{var} {type} {val}".format(
@@ -75,26 +74,27 @@ def step0(domain, *args, **kwargs):
             "/tmp/dokku-debconf",
         )
 
-        sudo("debconf-set-selections /tmp/dokku-debconf")
-        if not exists(key_file):
-            sudo(
+        c.sudo("debconf-set-selections /tmp/dokku-debconf")
+        if not exists(c, runner=c.run, path=key_file):
+            c.sudo(
                 'ssh-keygen -t rsa -b 4096 -f {key_file} -N ""'.format(
                     key_file=key_file
                 )
             )
 
-        apt_depends("dokku")
-        sudo("dokku plugin:install-dependencies --core")
-        put(local_pubkey, key_file)
-        sudo("sshcommand acl-add dokku domain {key_file}".format(key_file=key_file))
+        apt_depends(c, "dokku")
+        c.sudo("dokku plugin:install-dependencies --core")
+        c.put(local_pubkey, key_file)
+        c.sudo("sshcommand acl-add dokku domain {key_file}".format(key_file=key_file))
         return "installed dokku"
 
     if create_static:
-        if run("getent passwd static", quiet=True, warn_only=True).failed:
-            sudo("adduser static --disabled-password")
-            sudo("mkdir /home/static/sites/", user="static")
+        if c.run("getent passwd static", hide=True, warn=True).exited != 0:
+            c.sudo("adduser static --disabled-password")
+            c.sudo("mkdir /home/static/sites/", user="static")
 
-        upload_template(
+        upload_template_fmt(
+            c,
             path.join(
                 path.dirname(resource_filename("offregister_dokku", "__init__.py")),
                 "data",
@@ -104,32 +104,32 @@ def step0(domain, *args, **kwargs):
             use_sudo=True,
         )
 
-        if sudo("service nginx status").endswith("stop/waiting"):
-            sudo("service nginx start")
+        if c.sudo("service nginx status").stdout.endswith("stop/waiting"):
+            c.sudo("service nginx start")
         else:
-            sudo("service nginx reload")
+            c.sudo("service nginx reload")
 
         # TODO: Abstract this out into a different module, and allow for multiple domains
         if static_git_url:
             ipv4 = "/home/static/sites/{public_ipv4}".format(
                 public_ipv4=kwargs["public_ipv4"]
             )
-            if exists(ipv4):
-                sudo("rm -rf {ipv4}".format(ipv4=ipv4))
-            sudo("mkdir -p {ipv4}".format(ipv4=ipv4), user="static")
+            if exists(c, runner=c.run, path=ipv4):
+                c.sudo("rm -rf {ipv4}".format(ipv4=ipv4))
+            c.sudo("mkdir -p {ipv4}".format(ipv4=ipv4), user="static")
             if domain:
                 domain = "/home/static/sites/{domain}".format(domain=domain)
-                if not exists(domain):
-                    sudo(
+                if not exists(c, runner=c.run, path=domain):
+                    c.sudo(
                         "ln -s {ipv4} {domain}".format(ipv4=ipv4, domain=domain),
                         user="static",
                     )
             xip = "{ipv4}.xip.io".format(ipv4=ipv4)
-            if not exists(xip):
-                sudo("ln -s {ipv4} {xip}".format(ipv4=ipv4, xip=xip), user="static")
+            if not exists(c, runner=c.run, path=xip):
+                c.sudo("ln -s {ipv4} {xip}".format(ipv4=ipv4, xip=xip), user="static")
 
             if static_git_url:
-                apt_depends("git")
+                apt_depends(c, "git")
 
                 if isinstance(static_git_url, str):
                     clone_or_update(**url_to_git_dict(static_git_url))
@@ -140,5 +140,5 @@ def step0(domain, *args, **kwargs):
 
 
 def step1(*args, **kwargs):
-    run("echo serve")
+    c.run("echo serve")
     return "served dokku"
