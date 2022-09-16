620822a227e002d4d0a8b694f387f81d39f4cbb5
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-09-15 21:23:27 -0400

    [setup.py] Update to latest Python 2&3 compatible format

--- setup.py
+++ setup.py
@@ -1,25 +1,40 @@
-from sys import version
-
-from setuptools import find_packages, setup
-
-if version[0] == "2":
-    from itertools import imap as map, ifilter as filter
+# -*- coding: utf-8 -*-
 
 from ast import parse
 from distutils.sysconfig import get_python_lib
 from functools import partial
+from operator import attrgetter, itemgetter
 from os import listdir, path
+from sys import version_info
+
+from setuptools import find_packages, setup
+
+if version_info[0] == 2:
+    from itertools import ifilter as filter
+    from itertools import imap as map
 
 if __name__ == "__main__":
     package_name = "offregister_dokku"
 
     with open(path.join(package_name, "__init__.py")) as f:
         __author__, __version__ = map(
-            lambda buf: next(map(lambda e: e.value.s, parse(buf).body)),
-            filter(
-                lambda line: line.startswith("__version__")
-                or line.startswith("__author__"),
-                f,
+            lambda const: const.value if version_info > (3, 6) else const.s,
+            map(
+                attrgetter("value"),
+                map(
+                    itemgetter(0),
+                    map(
+                        attrgetter("body"),
+                        map(
+                            parse,
+                            filter(
+                                lambda line: line.startswith("__version__")
+                                or line.startswith("__author__"),
+                                f,
+                            ),
+                        ),
+                    ),
+                ),
             ),
         )
 
